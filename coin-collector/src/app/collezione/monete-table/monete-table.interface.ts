7040a7d7d69966524bed61dba70095d35a52c8be
Eugeniovinicio Berretta <euberdeveloper@gmail.com>
2019-02-14 22:35:49 +0100

    erased shit

--- coin-collector/src/app/collezione/monete-table/monete-table.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Moneta } from 'src/app/database/indexed-db.service';
-
-export interface Column {
-  name: string;
-  display: string;
-  value ?: (m: Moneta) => string;
-  footer ?: () => string;
-  paddingRight ? : boolean;
-}
